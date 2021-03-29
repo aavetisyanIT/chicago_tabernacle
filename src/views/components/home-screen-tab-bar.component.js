7249a45efeae2936fa0b843e5e737e98e0d7ec6f
aavetisyanit <aavetisyanit@gmail.com>
2021-03-29 17:19:47 -0500

    Fix bug with label on TopTabStack

--- src/views/components/home-screen-tab-bar.component.js
+++ src/views/components/home-screen-tab-bar.component.js
@@ -2,19 +2,22 @@ import * as React from 'react';
 import {Text, StyleSheet} from 'react-native';
 import {TabBar} from 'react-native-tab-view';
 
-const HomeScreenTabBar = props => (
-  <TabBar
-    {...props}
-    style={styles.tabBar}
-    indicatorStyle={{
-      backgroundColor: '#bc9665',
-      height: 2.5,
-    }}
-    renderLabel={({route}) => (
-      <Text style={styles.tabBarText}>{route.title}</Text>
-    )}
-  />
-);
+const HomeScreenTabBar = props => {
+  return (
+    <TabBar
+      {...props}
+      style={styles.tabBar}
+      indicatorStyle={{
+        backgroundColor: '#bc9665',
+        height: 2.5,
+      }}
+      renderLabel={({route}) => {
+        const tabLabel = route.name === 'News' ? "What's new" : 'Sermons';
+        return <Text style={styles.tabBarText}>{tabLabel}</Text>;
+      }}
+    />
+  );
+};
 export default HomeScreenTabBar;
 
 const styles = StyleSheet.create({
