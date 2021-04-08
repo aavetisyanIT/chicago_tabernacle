63aeff85012bb766175b925fe45d5915a27ef291
aavetisyanit <aavetisyanit@gmail.com>
2021-04-08 13:24:00 -0500

    create and style custom-card component

---
+++ src/views/components/custom-card.component.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const CustomCard = ({children}) => {
+  return (
+    <View style={styles.card}>
+      <View style={styles.cardContent}>{children}</View>
+    </View>
+  );
+};
+
+export default CustomCard;
+
+const styles = StyleSheet.create({
+  card: {
+    marginVertical: 12,
+    marginHorizontal: 12,
+
+    borderRadius: 5,
+    elevation: 5,
+    shadowOffset: {width: 1, height: 1},
+    shadowColor: '#333',
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+  },
+  cardContent: {
+    backgroundColor: '#fff',
+  },
+});
