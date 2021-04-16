d266b1548de20cba70a67cb6f68141780c60cd86
aavetisyanit <aavetisyanit@gmail.com>
2021-04-16 16:10:00 -0500

    Render flatlist with article paragraphs

--- src/views/screens/sermon-notes.tab.components.js
+++ src/views/screens/sermon-notes.tab.components.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import {Text, View, StyleSheet, Image} from 'react-native';
+import {Text, View, StyleSheet, Image, FlatList} from 'react-native';
+
+import SermonNotesParagraph from '../components/sermon-notes-paragraph';
 
 const SermonNotesTab = ({route}) => {
   const {article} = route.params;
   const imageUrl = article.image.url;
+
+  const PARAGRAPHSDATA = article.paragraphs;
+
+  const renderItem = props => <SermonNotesParagraph {...props} />;
+
   return (
     <View style={styles.container}>
-      <Image source={{uri: `${imageUrl}`}} style={styles.image} />
-      <View style={styles.headerContent}>
-        <Text style={styles.headLine}>{article.headline}</Text>
-        <Text style={styles.description}>{article.desc}</Text>
-      </View>
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <Image source={{uri: `${imageUrl}`}} style={styles.image} />
+            <View style={styles.headerContent}>
+              <Text style={styles.headLine}>{article.headline}</Text>
+              <Text style={styles.description}>{article.desc}</Text>
+              <Text style={styles.audioPlayer}>AUDIO COMPONENT</Text>
+            </View>
+          </>
+        }
+        data={PARAGRAPHSDATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </View>
   );
 };
@@ -21,6 +38,6 @@ const styles = StyleSheet.create({
   container: {flex: 1, backgroundColor: '#fff'},
   image: {height: 250, width: '100%'},
   headerContent: {padding: 13},
-  headLine: {fontFamily: 'Roboto-Medium'},
+  audioPlayer: {margin: 15, color: '#bc9665', fontFamily: 'Roboto-Medium'},
   description: {fontFamily: 'Roboto-Thin'},
 });
