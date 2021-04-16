ea95e108136a7701d9d421e947759c76e41f398e
aavetisyanit <aavetisyanit@gmail.com>
2021-04-15 20:26:10 -0500

    Install react-native-render-htm, and render static data from API

--- src/views/screens/devotional.tab.component.js
+++ src/views/screens/devotional.tab.component.js
@@ -1,12 +1,52 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, StyleSheet, useWindowDimensions} from 'react-native';
+import HTML from 'react-native-render-html';
 
-const DevotionalTab = () => {
+const htmlContent = `
+    <h2>
+  <span style="color: rgb(0, 0, 0); background-color: transparent"
+    >Jesus Will Build in the Context of Spiritual Opposition</span
+  >
+</h2>
+<h2><br /></h2>
+<h2>
+  <span style="color: rgb(0, 0, 0); background-color: transparent"
+    >Satan Will Oppose in the Context of Kingdom Progress</span
+  >
+</h2>
+<style>
+  * {
+    font-family: Proxima Nova;
+  }
+  p,
+  ul,
+  li,
+  ol {
+    font-size: 16px;
+  }
+  em {
+    font-style: italic;
+  }
+</style>
+
+`;
+
+const DevotionalTab = ({route}) => {
+  const contentWidth = useWindowDimensions().width;
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Devotional</Text>
+    <View style={styles.container}>
+      <Text style={styles.headLine}>Devotional Tab</Text>
+      <HTML source={{html: htmlContent}} contentWidth={contentWidth} />
     </View>
   );
 };
 
 export default DevotionalTab;
+
+const styles = StyleSheet.create({
+  container: {flex: 1, backgroundColor: '#fff'},
+  image: {height: 250, width: '100%'},
+  headerContent: {padding: 13},
+  headLine: {fontFamily: 'Roboto-Medium'},
+  description: {fontFamily: 'Roboto-Thin'},
+});
