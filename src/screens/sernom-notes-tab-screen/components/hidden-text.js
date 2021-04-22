9471c5786c94df32dc632df5ac4b2019d1c06cb2
aavetisyanit <aavetisyanit@gmail.com>
2021-04-21 23:42:23 -0500

    Create add-note and hidden-text components

---
+++ src/screens/sernom-notes-tab-screen/components/hidden-text.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const HiddenText = () => {
+  return (
+    <View>
+      <Text>Hidden Text Component</Text>
+    </View>
+  );
+};
+
+export default HiddenText;
