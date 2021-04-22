58a35f635ee3dbc13fd14366db48a22c79d31a90
aavetisyanit <aavetisyanit@gmail.com>
2021-04-21 23:43:19 -0500

    Conditionally render new components in sermon-note component

--- src/screens/sernom-notes-tab-screen/components/sermon-note.js
+++ src/screens/sernom-notes-tab-screen/components/sermon-note.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
+import AddNote from './add-note';
+import HiddenText from './hidden-text';
 import SermonNoteParagraphText from './sermon-note-paragraph-text';
 
 const SermonNote = ({item}) => {
   const PARAGRAPHHTML = item.text;
 
+  let paragraphContent = null;
+  if (item.actionType === 'button') {
+    paragraphContent = (
+      <SermonNoteParagraphText PARAGRAPHHTML={PARAGRAPHHTML} />
+    );
+  } else if (item.actionType === 'hiddenText') {
+    paragraphContent = <HiddenText />;
+  }
   return (
     <View style={styles.container}>
-      <SermonNoteParagraphText PARAGRAPHHTML={PARAGRAPHHTML} />
+      {paragraphContent}
+      {item.allowNotes ? <AddNote /> : null}
     </View>
   );
 };
