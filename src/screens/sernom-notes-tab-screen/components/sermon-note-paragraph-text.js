8683e6b7c2db07e761f7a7485a186530cdc94dc9
aavetisyanit <aavetisyanit@gmail.com>
2021-04-22 10:20:48 -0500

    Remove rendering sup tag cause superscript isn't suppored right now

--- src/screens/sernom-notes-tab-screen/components/sermon-note-paragraph-text.js
+++ src/screens/sernom-notes-tab-screen/components/sermon-note-paragraph-text.js
@@ -1,12 +1,25 @@
 import React from 'react';
-import {View, useWindowDimensions} from 'react-native';
+import {View, useWindowDimensions, Text} from 'react-native';
 import HTML from 'react-native-render-html';
 
 const SermonNoteParagraphText = ({PARAGRAPHHTML}) => {
   const contentWidth = useWindowDimensions().width;
+
+  const renderers = {
+    sup: ({__rawHtml}, children, convertedCSSStyles, passProps) => {
+      return <Text style={null} key={passProps.key} html={__rawHtml} />;
+    },
+  };
+
   return (
     <View>
-      <HTML source={{html: PARAGRAPHHTML}} contentWidth={contentWidth} />
+      <HTML
+        source={{
+          html: PARAGRAPHHTML,
+        }}
+        contentWidth={contentWidth}
+        renderers={renderers}
+      />
     </View>
   );
 };
