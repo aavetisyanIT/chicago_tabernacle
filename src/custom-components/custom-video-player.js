5222781ce5db51528d734a0f51c91cc386b9f68f
aavetisyanit <aavetisyanit@gmail.com>
2021-06-01 11:59:32 -0500

    Add forward, play/pause, backward buttons

--- src/custom-components/custom-video-player.js
+++ src/custom-components/custom-video-player.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import {StyleSheet, Text, View, Dimensions} from 'react-native';
 import Video from 'react-native-video';
-import {Icon} from 'react-native-vector-icons/FontAwesome5';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 const {width} = Dimensions.get('window');
 
 const CustomVideoPlayer = ({videoUrl, imageUrl}) => {
-  const [paused, setPaused] = React.useState(true);
+  const [paused, setPaused] = React.useState(false);
+  const [overlay, setOverlay] = React.useState(false);
   return (
     <View style={styles.container}>
       <View style={styles.videoPlayerContainer}>
         <Video
-          paused={paused}
-          ref={ref => (Video.player = ref)}
+          paused={!paused}
+          style={{...StyleSheet.absoluteFill}}
           source={{uri: videoUrl}}
           resizeMode="cover"
           poster={imageUrl}
-          style={{...StyleSheet.absoluteFill}}
+          ref={ref => (Video.player = ref)}
+          onLoad={Video.load}
+          onProgress={Video.progress}
         />
-        <View style={styles.overlay}></View>
+        <View style={styles.overlay}>
+          {!overlay ? (
+            //shade effect
+            <View style={{...styles.overlaySet, backgroundColor: '#0006'}}>
+              {/* Controllers */}
+              {/* backward is not working */}
+              <Icon
+                name="backward"
+                style={styles.icon}
+                onPress={Video.backward}
+              />
+              <Icon
+                name={paused ? 'play' : 'pause'}
+                style={styles.icon}
+                onPress={() => setPaused(currentPaused => !currentPaused)}
+              />
+              {/* forward is not working */}
+              <Icon
+                name="forward"
+                style={styles.icon}
+                onPress={Video.forward}
+              />
+            </View>
+          ) : (
+            <View style={styles.overlaySet}></View>
+          )}
+        </View>
       </View>
     </View>
   );
@@ -34,4 +63,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
   },
   overlay: {...StyleSheet.absoluteFill},
+  overlaySet: {
+    flex: 1,
+    flexDirection: 'row',
+  },
+  icon: {
+    color: 'white',
+    flex: 1,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    fontSize: 25,
+  },
 });
