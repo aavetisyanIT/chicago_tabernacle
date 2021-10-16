28fd8ec759cfc659d6e7ae0fdb98cc79fe8b7c96
aavetisyanit <aavetisyanit@gmail.com>
2021-10-15 23:17:01 -0500

    Initialize google authentication and create signin function

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {Provider as PaperProvider} from 'react-native-paper';
 import {NavigationContainer} from '@react-navigation/native';
-/*
 import auth from '@react-native-firebase/auth';
 import {GoogleSignin} from '@react-native-community/google-signin';
-*/
+
 import {navigationRef} from './navigation/RootNavigation';
 import RootStack from './navigation/RootStack';
 import {reactNativePaperTheme} from './config/react-native-paper-theme';
 import {AppContextProvider} from './context/app.context.provider';
-/*
+
 GoogleSignin.configure({
   webClientId: `1027043919366 - orboquk60q94tb85eds13i1e3e147tkg.apps.googleusercontent.com`,
 });
@@ -20,7 +19,6 @@ const onGoogleButtonPress = async () => {
   const googleCredential = auth.GoogleAuthProvider.credential(idToken);
   return auth().signInWithCredential(googleCredential);
 };
-*/
 
 export default function App() {
   return (
