50174f67115dbd274470fcb7115d5ad3a062330c
aavetisyanit <aavetisyanit@gmail.com>
2021-12-06 15:22:03 -0600

    Add icons to DrawerContent component

--- src/navigation/DrawerContent.js
+++ src/navigation/DrawerContent.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {View, StyleSheet, Linking} from 'react-native';
 import {DrawerItem, DrawerContentScrollView} from '@react-navigation/drawer';
 import {Drawer} from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons';
+import HandHoldingHeartIcon from 'react-native-vector-icons/FontAwesome5';
+import ExchangeArrowsIcon from 'react-native-vector-icons/FontAwesome5';
 
 import * as RootNavigation from './RootNavigation';
 import {AppContext} from './../context/app.context';
@@ -16,6 +19,18 @@ const DrawerContent = props => {
     getCurrentUserInfo();
   }, []);
 
+  const createDrawerIcon = name => {
+    return <Icon name={name} color="black" size={22} />;
+  };
+  const createHandHoldingHeartIcon = () => {
+    return (
+      <HandHoldingHeartIcon name="hand-holding-heart" color="black" size={22} />
+    );
+  };
+  const createExchangeArrowsIcon = () => {
+    return <ExchangeArrowsIcon name="exchange-alt" color="black" size={22} />;
+  };
+
   return (
     <DrawerContentScrollView
       {...props}
@@ -27,10 +42,14 @@ const DrawerContent = props => {
         <Drawer.Section style={styles.drawerSection}>
           <DrawerItem
             label="WHAT'S NEW"
+            labelStyle={{color: 'black'}}
+            icon={() => createDrawerIcon('flag')}
             onPress={() => RootNavigation.navigate("WHAT'S NEW")}
           />
           <DrawerItem
             label="SERMONS"
+            labelStyle={{color: 'black'}}
+            icon={() => createDrawerIcon('bookmark')}
             onPress={() => {
               RootNavigation.navigate('SERMONS');
             }}
@@ -39,27 +58,44 @@ const DrawerContent = props => {
         <Drawer.Section>
           <DrawerItem
             label="Prayer Request"
+            labelStyle={{color: 'black'}}
+            icon={createExchangeArrowsIcon}
             onPress={() => RootNavigation.navigate('PrayrRequest')}
           />
           <DrawerItem
             label="Give"
+            icon={() => createDrawerIcon('heart')}
             onPress={() =>
               Linking.openURL('https://www.chicagotabernacle.org/give/')
             }
           />
-          <DrawerItem label="Ministries" onPress={() => {}} />
-          <DrawerItem label="Serve" onPress={() => {}} />
-          <DrawerItem label="About" onPress={() => {}} />
+          <DrawerItem
+            label="Ministries"
+            icon={() => createDrawerIcon('people')}
+            onPress={() => {}}
+          />
+          <DrawerItem
+            label="Serve"
+            icon={createHandHoldingHeartIcon}
+            onPress={() => {}}
+          />
+          <DrawerItem
+            icon={() => createDrawerIcon('location-sharp')}
+            label="About"
+            onPress={() => {}}
+          />
         </Drawer.Section>
         <Drawer.Section>
           <DrawerItem
             label="Facebook"
+            icon={() => createDrawerIcon('ios-logo-facebook')}
             onPress={() =>
               Linking.openURL('https://www.facebook.com/chicagotab/')
             }
           />
           <DrawerItem
             label="Instagram"
+            icon={() => createDrawerIcon('ios-logo-instagram')}
             onPress={() =>
               Linking.openURL('https://www.instagram.com/chicagotab/')
             }
